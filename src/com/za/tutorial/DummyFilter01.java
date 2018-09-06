05a775e96f565c2a2ac5920b72aa8cc6e032e014
Israel Mesa <mesa.israel50@gmail.com>
2018-09-06 15:48:01 -0400

    ServletFilterTutorial

---
+++ src/com/za/tutorial/DummyFilter01.java
@@ -0,0 +1,22 @@
+package com.za.tutorial;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class DummyFilter01 implements Filter {
+
+
+	public void destroy() {}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("DummyFilter01.doFilter01...");
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {}
+
+}
